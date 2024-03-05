e69231e90de57527c0e5dd6b48d396aacc80a6de
hamedagh <hameddevflutter@gmail.com>
2024-03-05 15:08:10 +0330

    authen

---
+++ ghahvehApiApp/authen.py
@@ -0,0 +1,11 @@
+# middleware.py
+from django.http import JsonResponse
+
+class AuthenticationMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if not request.user.is_authenticated:
+            return JsonResponse({'error': 'Authentication required'}, status=401)
+        return self.get_response(request)
